9327c226aaab827cd0929da93b5e553fd0452590
황진태 <jintea519@gmail.com>
2023-01-31 16:51:52 +0900

    FEAT Set Default Screens

--- Develop/FE_Main/saffytime/lib/screens/counsel_screen.dart
+++ Develop/FE_Main/saffytime/lib/screens/counsel_screen.dart
@@ -1,1 +1,22 @@
 // check and regist counsel screen
+
+import 'package:flutter/material.dart';
+
+class CounselScreen extends StatefulWidget {
+  const CounselScreen({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _CounselScreenStates();
+}
+
+class _CounselScreenStates extends State<CounselScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [Text("CounselScreen")],
+      ),
+    );
+  }
+}
--- Develop/FE_Main/saffytime/lib/screens/home_screen.dart
+++ Develop/FE_Main/saffytime/lib/screens/home_screen.dart
@@ -1,1 +1,22 @@
 // main screen
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+  @override
+  State<StatefulWidget> createState() => _HomeScreenStates();
+}
+
+class _HomeScreenStates extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [Text("HomeScreen")],
+      ),
+    );
+  }
+}
