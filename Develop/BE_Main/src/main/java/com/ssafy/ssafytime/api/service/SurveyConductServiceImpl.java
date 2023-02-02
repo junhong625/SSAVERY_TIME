c305a22e893403600098bf0e0726748405792006
최재희 <withfavor98@naver.com>
2023-02-02 12:55:23 +0900

    FEAT ì메인페이지에 띄울 설문들조회

--- Develop/BE_Main/src/main/java/com/ssafy/ssafytime/api/service/SurveyConductServiceImpl.java
+++ Develop/BE_Main/src/main/java/com/ssafy/ssafytime/api/service/SurveyConductServiceImpl.java
@@ -1,9 +1,13 @@
 package com.ssafy.ssafytime.api.service;
 
+import com.ssafy.ssafytime.db.entity.Survey;
 import com.ssafy.ssafytime.db.entity.SurveyConduct;
+import com.ssafy.ssafytime.db.entity.User;
 import com.ssafy.ssafytime.db.repository.SurveyConductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service("surveyConductService")
 public class SurveyConductServiceImpl implements SurveyConductService {
@@ -13,5 +17,10 @@ public class SurveyConductServiceImpl implements SurveyConductService {
     @Override
     public void save(SurveyConduct surveyConduct) {
         surveyConductRepository.save(surveyConduct);
+    }
+
+    @Override
+    public Optional<Survey> findByUserIdxAndSurveyIdx(User user, Survey survey) {
+        return surveyConductRepository.findByUserIdxAndSurveyIdx(user, survey);
     }
 }
--- Develop/BE_Main/src/main/java/com/ssafy/ssafytime/api/service/SurveyService.java
+++ Develop/BE_Main/src/main/java/com/ssafy/ssafytime/api/service/SurveyService.java
@@ -14,6 +14,6 @@ public interface SurveyService {
 
     Optional<Survey> findById(Long Id);
 
-//    List<Survey> findByStatus(Integer Status);
+    List<Survey> findAllByStatus(Integer Status);
     void save(Survey survey);
 }
