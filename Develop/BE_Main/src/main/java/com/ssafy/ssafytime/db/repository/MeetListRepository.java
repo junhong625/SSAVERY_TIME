5c3a8161573ae86eab3de71226b7e5672c82b727
cheongwonyoung <kaka491@naver.com>
2023-02-05 15:43:35 +0900

    MOD 주석 추가

--- Develop/BE_Main/src/main/java/com/ssafy/ssafytime/db/repository/MeetListRepository.java
+++ Develop/BE_Main/src/main/java/com/ssafy/ssafytime/db/repository/MeetListRepository.java
@@ -11,12 +11,17 @@ import java.util.Optional;
 
 @Repository
 public interface MeetListRepository extends JpaRepository<MeetList,Long> {
+    // 교육생 번호에 해당하는 상담리스트 조회
     List<MeetList> findByStudentId(Long userId);
+    // 매니저 번호에 해당하는 상담리스트 조회
     List<MeetList> findByManagerId(Long userId);
+    // 상담 idx에 해당하는 상담 조회
     MeetList findByRezIdx(Long rezIdx);
+    // 예약 날짜와 매니저 번호에 해당하는 상담 리스트 조회
     List<MeetList> findByRezDateAndManagerId(LocalDate rezDate , long managerId);
-
+    // 학생 번호에 해당하는 상담리스트와 상대 이름 조회
     List<MeetList> findAllByStudentId_UserIdx(Long userId);
+    // 학생 번호에 해당하는 상담리스트와 상대 이름 조회
     List<MeetList> findAllByManagerId_UserIdx(Long ManagerId);
 }
 
