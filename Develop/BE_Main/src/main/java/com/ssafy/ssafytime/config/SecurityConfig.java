daba7387a5b3472c20bcc2a8e1dfdac978e652da
ktyoo226 <ktyoo226@naver.com>
2023-02-06 13:49:21 +0900

    update : permitAll

--- Develop/BE_Main/src/main/java/com/ssafy/ssafytime/config/SecurityConfig.java
+++ Develop/BE_Main/src/main/java/com/ssafy/ssafytime/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeHttpRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/**").permitAll()
 
                 .anyRequest().authenticated()
 
