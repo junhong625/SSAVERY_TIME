18ba15d85873ffe44854b68edeeed9380d35f84f
최재희 <withfavor98@naver.com>
2023-02-09 14:16:06 +0900

    FEAT FCM about consulting

--- Develop/BE_Main/src/main/java/com/ssafy/ssafytime/api/controller/MeetController.java
+++ Develop/BE_Main/src/main/java/com/ssafy/ssafytime/api/controller/MeetController.java
@@ -1,9 +1,15 @@
 package com.ssafy.ssafytime.api.controller;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Notification;
+import com.ssafy.ssafytime.api.firebase.MessageDTO;
+import com.ssafy.ssafytime.api.service.AlarmDefaultService;
 import com.ssafy.ssafytime.api.service.MeetService;
 import com.ssafy.ssafytime.db.dto.MeetInfoDto;
 import com.ssafy.ssafytime.db.entity.MeetList;
+import com.ssafy.ssafytime.db.repository.AlarmDefaultRepository;
 import com.ssafy.ssafytime.db.repository.MeetListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +21,12 @@ import java.util.List;
 @RestController
 @RequestMapping("/meet")
 public class MeetController {
+
+    @Autowired
+    AlarmDefaultService alarmDefaultService;
+
+    @Autowired
+    AlarmDefaultRepository alarmDefaultRepository;
     private final MeetService meetService;
     private final MeetListRepository meetListRepository;
 
@@ -51,7 +63,7 @@ public class MeetController {
     // 거절사유 등록 ( 매니저가 등록 )
     // rez_idx는 예약된 상담의 번호, reject는 거절 사유
     @PutMapping("/update/reject")
-    public void putReject(@RequestParam("rez_idx") Long rezIdx, @RequestParam("reject") String reject) {
+    public ResponseEntity<String> putReject(@RequestParam("rez_idx") Long rezIdx, @RequestParam("reject") String reject) throws FirebaseMessagingException {
 
         // 예약된 상담의 정보 가져오기
         MeetList member = meetService.findByRezIdx(rezIdx);
@@ -61,12 +73,26 @@ public class MeetController {
         member.setReject(reject);
         meetService.update(member);
 
+
+        Double doubleTime = 0.0;  // 상담 알림 메시지 위함
+        String time = null;
+        if(member.getRezTime()%1.0 == 0.5)  // 시간이 16.5 면 16:30으로 바꾸게!
+            time = String.valueOf(doubleTime) + ":30";
+        else
+            time = String.valueOf(doubleTime) + ":00";
+
+        // 알림 보내기
+        MessageDTO messageDTO = MessageDTO.builder().title("상담 알림").body(member.getRezDate() + " " + time + " 상담 거절됨").build();  // 알림에 넣을 인자들
+        List<String> registrationTokens = alarmDefaultService.getUserTokens(3, messageDTO);  // 1 : 설문, 2 : 공지 , 3: 상담 으로 설정하여 알림보낼 유저들 토큰 얻는 함수
+        Notification notification = Notification.builder().setTitle(messageDTO.getTitle()).setBody(messageDTO.getBody()).setImage(null).build();  // 없으면 알림 안보내짐
+        Integer FailedAlarmCnt = alarmDefaultService.sendMultiAlarms(notification, registrationTokens);
+        return ResponseEntity.ok().body("FailedAlarmCnt : " + FailedAlarmCnt);
     }
 
     // 상담승인 등록 ( 매니저가 등록 )
     // rez_idx는 예약된 상담의 번호
     @PutMapping("/update/accept")
-    public void putReject(@RequestParam("rez_idx") Long rezIdx) {
+    public ResponseEntity<String> putAccept(@RequestParam("rez_idx") Long rezIdx) throws FirebaseMessagingException {
 
         // 예약된 상담의 정보 가져오기
         MeetList member = meetService.findByRezIdx(rezIdx);
@@ -74,6 +100,19 @@ public class MeetController {
         member.setState(2L);
         meetService.update(member);
 
+        Double doubleTime = 0.0;  // 상담 알림 메시지 위함
+        String time = null;
+        if(member.getRezTime()%1.0 == 0.5)  // 시간이 16.5 면 16:30으로 바꾸게!
+            time = String.valueOf(doubleTime) + ":30";
+        else
+            time = String.valueOf(doubleTime) + ":00";
+
+        // 알림 보내기
+        MessageDTO messageDTO = MessageDTO.builder().title("상담 승인 알림").body(member.getRezDate() + " " + time + " 상담 거절됨").build();  // 알림에 넣을 인자들
+        List<String> registrationTokens = alarmDefaultService.getUserTokens(3, messageDTO);  // 1 : 설문, 2 : 공지 , 3: 상담 으로 설정하여 알림보낼 유저들 토큰 얻는 함수
+        Notification notification = Notification.builder().setTitle(messageDTO.getTitle()).setBody(messageDTO.getBody()).setImage(null).build();  // 없으면 알림 안보내짐
+        Integer FailedAlarmCnt = alarmDefaultService.sendMultiAlarms(notification, registrationTokens);
+        return ResponseEntity.ok().body("FailedAlarmCnt : " + FailedAlarmCnt);
     }
 
 }
