6ee8472092dd464714a8d1a247696e496d0181df
ktyoo226 <ktyoo226@naver.com>
2023-02-10 17:30:27 +0900

    update : AttendanceRepository sql change

--- Develop/BE_Main/src/main/java/com/ssafy/ssafytime/db/repository/AttendanceRepository.java
+++ Develop/BE_Main/src/main/java/com/ssafy/ssafytime/db/repository/AttendanceRepository.java
@@ -27,7 +27,7 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Attendan
 
     @Query(value = "select a.user_idx,  2 as \"category\" ,count(a.attendance_category) as count " +
             "from attendance a  " +
-            "where a.user_idx = 1 and month(now()) = month(a.attendance_date) and (a.attendance_category !=0 and a.attendance_category != 1);", nativeQuery = true)
+            "where a.user_idx = 1 and month(now()) = month(a.attendance_date) and (a.attendance_category != 1);", nativeQuery = true)
     List<AttendanceInterface>findAllAttendance(@Param("userIdx") Long userIdx);
 
 
