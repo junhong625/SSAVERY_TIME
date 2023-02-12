3e40be603fe9756a6b7041ff1c85ee875661b1b4
ktyoo226 <ktyoo226@naver.com>
2023-02-13 01:28:20 +0900

    feat : create createTemporaryPassword API

--- Develop/BE_Main/src/main/java/com/ssafy/ssafytime/api/service/AuthService.java
+++ Develop/BE_Main/src/main/java/com/ssafy/ssafytime/api/service/AuthService.java
@@ -3,8 +3,11 @@ package com.ssafy.ssafytime.api.service;
 import com.ssafy.ssafytime.db.dto.TokenDto;
 import com.ssafy.ssafytime.db.dto.TokenRequest;
 import com.ssafy.ssafytime.db.dto.TokenResponse;
+import com.ssafy.ssafytime.db.repository.RefreshTokenRepository;
 import com.ssafy.ssafytime.jwt.RefreshTokenValidator;
 import com.ssafy.ssafytime.jwt.TokenProvider;
+import com.ssafy.ssafytime.util.SecurityUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,21 +15,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
     private final RefreshTokenValidator refreshTokenValidator;
     private final TokenProvider tokenProvider;
     private final TokenService tokenService;
 
-    public AuthService(
-        final RefreshTokenValidator refreshTokenValidator,
-        final TokenProvider tokenProvider,
-        final TokenService tokenService
-    ){
+    private final RefreshTokenRepository refreshTokenRepository;
 
-        this.refreshTokenValidator = refreshTokenValidator;
-        this.tokenService = tokenService;
-        this.tokenProvider = tokenProvider;
-    }
 
     @Transactional
     public TokenResponse refreshToken(final TokenRequest tokenRequest){
@@ -51,6 +47,20 @@ public class AuthService {
             tokenService.invalidateRefreshToken(userIdx);
             throw new BadCredentialsException("리프레쉬 토큰이 유효하지 않습니다.");
         }
+    }
+
+    @Transactional
+    public void logout(TokenRequest tokenRequest){
+
+        SecurityUtil.getCurrentUsername();
+
+        final Authentication authentication = tokenProvider.getAuthentication(tokenRequest.getAccessToken());
+
+        System.out.println(authentication.getPrincipal());
+
+//        String userEmail = authentication.getName();
+
+//        refreshTokenRepository.findRefreshTokenByUserEmail(userEmail);
     }
 
 
