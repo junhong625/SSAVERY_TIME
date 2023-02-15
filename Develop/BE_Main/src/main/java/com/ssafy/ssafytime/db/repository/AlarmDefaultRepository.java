96272787685b1d475e0e77007ca9c1c6edd9365b
junhong625 <junhong625@naver.com>
2023-02-15 13:20:09 +0900

    UPDATE [알림] update api

--- Develop/BE_Main/src/main/java/com/ssafy/ssafytime/db/repository/AlarmDefaultRepository.java
+++ Develop/BE_Main/src/main/java/com/ssafy/ssafytime/db/repository/AlarmDefaultRepository.java
@@ -3,8 +3,11 @@ package com.ssafy.ssafytime.db.repository;
 import com.ssafy.ssafytime.db.entity.AlarmDefault;
 import com.ssafy.ssafytime.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,4 +19,19 @@ public interface AlarmDefaultRepository extends JpaRepository<AlarmDefault, Long
     List<AlarmDefault> findAllByConsultingAlarm(boolean chk);
 
     List<AlarmDefault> findAllByNoticeAlarm(boolean chk);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE AlarmDefault SET noticeAlarm = case when noticeAlarm = true then false else true end WHERE userId = :userId")
+    void noticeUpdate(Long userId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE AlarmDefault SET surveyAlarm = case when surveyAlarm = true then false else true end WHERE userId = :userId")
+    void surveyUpdate(Long userId);
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE AlarmDefault SET consultingAlarm = case when consultingAlarm = true then false else true end WHERE userId = :userId")
+    void consultingUpdate(Long userId);
 }
