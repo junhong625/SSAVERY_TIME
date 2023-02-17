11c60d2fd7cb75b6f613dfb16e4eceeb48d387e1
황진태 <jintea519@gmail.com>
2023-02-17 11:41:53 +0900

    Fix bug

--- Develop/FE_Main/ssafytime/lib/screens/survey_screen.dart
+++ Develop/FE_Main/ssafytime/lib/screens/survey_screen.dart
@@ -5,7 +5,6 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
-import 'package:ssafytime/controllers/loading_controller.dart';
 import 'package:ssafytime/controllers/survey_controller.dart';
 import 'package:ssafytime/flutter_survey-0.1.4/flutter_survey.dart';
 
@@ -32,7 +31,6 @@ class _SurveyScreen extends State<SurveyScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Get.find<loadingController>().setIsLoading(true);
     log("${surveyC}");
     return Obx(
       () => Scaffold(
